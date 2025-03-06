0158a17ae08341b5bcb95b4601e6198c87155a0c
Rolex-444 <sivaraman.gc49@gmail.com>
2025-03-06 16:32:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -27,7 +27,7 @@ PORT = os.environ.get("PORT", "8080")
 
 #Database 
 DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://sivaraman:siva444@cluster0.bhuvc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-DB_NAME = os.environ.get("DATABASE_NAME", "Cluster0")
+DB_NAME = os.environ.get("DATABASE_NAME", "cluster0")
 
 #force sub channel id, if you want enable force sub
 FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "0"))
